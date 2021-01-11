d034359fe879fc58c40e19b4a4ddf5609912e0a9
John Watson <johnwatson484@hotmail.com>
2021-01-11 20:19:56 +0000

    Removed defunct config

--- app/config/index.js
+++ app/config/index.js
@@ -16,14 +16,6 @@ const config = {
     password: process.env.AZURE_SERVICE_BUS_PASSWORD,
     address: process.env.AZURE_SERVICE_BUS_TOPIC,
     type: 'topic'
-  },
-  subscriptionConfig: {
-    host: process.env.AZURE_SERVICE_BUS_HOST,
-    username: process.env.AZURE_SERVICE_BUS_USERNAME,
-    password: process.env.AZURE_SERVICE_BUS_PASSWORD,
-    address: process.env.AZURE_SERVICE_BUS_SUBSCRIPTION,
-    topic: process.env.AZURE_SERVICE_BUS_TOPIC,
-    type: 'subscription'
   }
 }
 
